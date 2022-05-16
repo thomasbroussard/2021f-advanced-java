98477e6338aab2a36e435deb24cc52f767abba26
Présentation <thomas.broussard@epita.fr>
2022-05-16 12:07:27 +0200

    * complete the PassengerDAOTest and fix the errors in the executions

--- junit-demo/src/test/java/fr/epita/junit/demo/services/PassengerDAO.java
+++ junit-demo/src/test/java/fr/epita/junit/demo/services/PassengerDAO.java
@@ -14,8 +14,12 @@ public class PassengerDAO {
     public void insert(Passenger passenger) throws SQLException {
         Connection connection = DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "username", "password");
         PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO PASSENGERS(name,age,pclass, survived, gender) values(?,?,?,?,?)");
-        // parameters mapping TODO
-        // preparedStatement.setString();
+
+        preparedStatement.setString(1, passenger.getName());
+        preparedStatement.setInt(2, passenger.getAge());
+        preparedStatement.setInt(3, passenger.getPclass());
+        preparedStatement.setBoolean(4 , passenger.isSurvived());
+        preparedStatement.setInt(5, passenger.getGender());
         preparedStatement.execute();
     }
 }
--- junit-demo/src/test/java/fr/epita/junit/demo/services/PassengerDAOTest.java
+++ junit-demo/src/test/java/fr/epita/junit/demo/services/PassengerDAOTest.java
@@ -1,18 +1,28 @@
 package fr.epita.junit.demo.services;
 
 import fr.epita.junit.demo.datamodel.Passenger;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class PassengerDAOTest {
 
 
+    private Connection cnt;
+
+    @BeforeEach
+    public void initialize() throws SQLException {
+        this.cnt = DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "username", "password");
+
+        PreparedStatement preparedStatement = cnt.prepareStatement("CREATE TABLE IF NOT EXISTS PASSENGERS  (name VARCHAR(255), age INT, pclass INT, survived BOOLEAN, gender INT)");
+        preparedStatement.execute();
+    }
+
+
     @Test
     public void test() throws SQLException {
+
         //given
         Passenger passenger = new Passenger("test", 30, 3, true, 1);
 
@@ -22,7 +32,7 @@ public class PassengerDAOTest {
 
 
         //then
-        Connection connection = DriverManager.getConnection("");
+        Connection connection =  cnt;
         ResultSet resultSet = connection.prepareStatement("select * from passengers where name = 'test'").executeQuery();
         String retrievedName = null;
         while (resultSet.next()) {
